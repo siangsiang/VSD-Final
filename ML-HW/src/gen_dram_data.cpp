fae33a3f154c55be975600c1233efbfdefe4c9dc
CW-B-W <cw.brad.wang@gmail.com>
2022-01-12 19:24:05 +0000

    Fix bugs

--- ML-HW/src/gen_dram_data.cpp
+++ ML-HW/src/gen_dram_data.cpp
@@ -67,14 +67,14 @@ int main(int argc, char* argv[])
         }
     }
 
-    int num[4] = {10, 100, 1000, 10000};
+    int num[4] = {10, 100, 1000, 8192};
     for (int n = 0; n < 4; ++n) {
         int t = 0;
-        fstream ofs_b3(string("dram_data/input/input_")+to_string(t)+"_b3.hex", ios::out);
-        fstream ofs_b2(string("dram_data/input/input_")+to_string(t)+"_b2.hex", ios::out);
-        fstream ofs_b1(string("dram_data/input/input_")+to_string(t)+"_b1.hex", ios::out);
-        fstream ofs_b0(string("dram_data/input/input_")+to_string(t)+"_b0.hex", ios::out);
-        fstream ofs_golden(string("dram_data/golden/golden_")+to_string(t)+".hex", ios::out);
+        fstream ofs_b3(string("dram_data/input/input_")+to_string(num[n])+"_b3.hex", ios::out);
+        fstream ofs_b2(string("dram_data/input/input_")+to_string(num[n])+"_b2.hex", ios::out);
+        fstream ofs_b1(string("dram_data/input/input_")+to_string(num[n])+"_b1.hex", ios::out);
+        fstream ofs_b0(string("dram_data/input/input_")+to_string(num[n])+"_b0.hex", ios::out);
+        fstream ofs_golden(string("dram_data/golden/golden_")+to_string(num[n])+".hex", ios::out);
         vector<int> cls_idx(10, 0);
         bool done = false;
         while (!done) {
