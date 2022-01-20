62e5f3f73d287cfea7fb2abc524e4a1a1d9865d9
CW-B-W <cw.brad.wang@gmail.com>
2022-01-20 18:36:50 +0000

    Add code to generate verilog ML parameters

---
+++ ML-HW/data/weights/gencode_conv.py
@@ -0,0 +1,35 @@
+conv_id = 0
+for kernel_id in range(5):
+    filename = f'Conv1d(1, 5, kernel_size=(5,), stride=(2,), bias=False)_{kernel_id}.csv'
+    with open(filename) as rf:
+        while True:
+            str = rf.readline()
+            if str == '':
+                break
+            str = str[:str.find('\n')].split(',')
+            print(f'//conv{conv_id} kernel{kernel_id} coeff')
+            print(f'wire signed[15:0] conv{conv_id}_kernel{kernel_id}_c[4:0];')
+            for idx in range(0, len(str)):
+                val = str[idx]
+                assign_str = f'assign conv{conv_id}_kernel{kernel_id}_c[{idx}] = 16\'h{val};'
+                print(assign_str)
+            print('\n')
+
+conv_id = 1
+for kernel_id in range(2):
+    filename = f'Conv1d(5, 2, kernel_size=(5,), stride=(2,), bias=False)_{kernel_id}.csv'
+    with open(filename) as rf:
+        channel_id = 0
+        while True:
+            str = rf.readline()
+            if str == '':
+                break
+            str = str[:str.find('\n')].split(',')
+            print(f'//conv{conv_id} kernel{kernel_id} coeff')
+            print(f'wire signed[15:0] conv{conv_id}_kernel{kernel_id}_{channel_id}_c[4:0];')
+            for idx in range(0, len(str)):
+                val = str[idx]
+                assign_str = f'assign conv{conv_id}_kernel{kernel_id}_{channel_id}_c[{idx}] = 16\'h{val};'
+                print(assign_str)
+            print('\n')
+            channel_id += 1
---
+++ ML-HW/data/weights/gencode_fc.py
@@ -0,0 +1,31 @@
+fc_id = 0
+filename = 'Linear(in_features=30, out_features=9, bias=False).csv'
+with open(filename) as rf:
+    for kernel_id in range(0, 9):
+        str = rf.readline()
+        if str == '':
+            break
+        str = str[:str.find('\n')].split(',')
+        print(f'//FC{fc_id}_node{kernel_id}')
+        print(f'wire signed[15:0] fc{fc_id}_node{kernel_id}_c[29:0];')
+        for idx in range(0, len(str)):
+            val = str[idx]
+            assign_str = f'assign fc{fc_id}_node{kernel_id}_c[{idx}] = 16\'h{val};'
+            print(assign_str)
+        print('\n')
+
+fc_id = 1
+filename = 'Linear(in_features=9, out_features=5, bias=False).csv'
+with open(filename) as rf:
+    for kernel_id in range(0, 5):
+        str = rf.readline()
+        if str == '':
+            break
+        str = str[:str.find('\n')].split(',')
+        print(f'//FC{fc_id}_node{kernel_id}')
+        print(f'wire signed[15:0] fc{fc_id}_node{kernel_id}_c[8:0];')
+        for idx in range(0, len(str)):
+            val = str[idx]
+            assign_str = f'assign fc{fc_id}_node{kernel_id}_c[{idx}] = 16\'h{val};'
+            print(assign_str)
+        print('\n')
