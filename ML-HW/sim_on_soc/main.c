570826522ac77641b990c22840f12c527b1169e9
CW-B-W <cw.brad.wang@gmail.com>
2022-01-24 22:16:36 +0800

    Fix compilation error

--- ML-HW/sim_on_soc/main.c
+++ ML-HW/sim_on_soc/main.c
@@ -1,11 +1,16 @@
 #include "sim.h"
+#include "fixed32.h"
 
 extern unsigned int _test_start;
 
 volatile unsigned int *ecg_addr  = (volatile unsigned int *) 0x20140000; // DRAM @50000
 
+void result() {
+
+}
+
 int main(void) {
-  unsigned int buf[360];
+  fixed32 buf[360];
   for (int i = 0; i < 360; ++i) {
     buf[i] = ecg_addr[i];
   }
